e1802f64b06eb23ffdcd90683644ed15d4b4b6af
typoman <eslami.bahman@gmail.com>
2023-02-07 12:27:23 +0100

    add a installer for pip.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fontGadgets",
+    version="0.0.2",
+    description="A package to add more functions to fontParts and defcon",
+    author="Bahman Eslami",
+    author_email="contact@bahman.design",
+    url="http://bahman.design",
+    license="MIT",
+    platforms=["Any"],
+    package_dir={'': 'fontgadgets.roboFontExt/lib'},
+    packages=find_packages('fontgadgets.roboFontExt/lib'),
+    install_requires=[
+    "fontParts",
+    "fontTools",
+    "ufo2ft",
+    "ufoLib2",
+    "pytest",
+    "ufonormalizer"
+    ],
+    tests_require=[
+        'pytest>=3.7',
+    ],
+)
