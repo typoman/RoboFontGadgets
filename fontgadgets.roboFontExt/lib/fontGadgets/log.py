3e08d89d09b1c55e311d9f2f4a5d1c1cb133742f
typoman <eslami.bahman@gmail.com>
2024-04-29 17:12:37 +0200

    Add log.

---
+++ fontgadgets.roboFontExt/lib/fontGadgets/log.py
@@ -0,0 +1,32 @@
+import logging
+import sys
+
+logging.addLevelName(logging.ERROR, '| FAILED ')
+logging.addLevelName(logging.WARNING, '| WARNING ')
+logging.addLevelName(logging.INFO, '')
+logging.addLevelName(logging.DEBUG, '')
+
+
+class MergerLogFormatter(logging.Formatter):
+	COLORS = {
+			logging.WARNING: '1;30;44',
+			logging.ERROR: '1;30;41',
+			logging.DEBUG: '0;30;46',
+			logging.INFO: '0;30;42',
+			}
+
+	COLOR_SEQ = "\x1b[1;%sm"
+
+	def format(self, record):
+		levelno = record.levelno
+		color = self.COLOR_SEQ % self.COLORS[levelno]
+		record.module = color + " " + record.module.upper()
+		record.levelname = color + record.levelname + "\x1b[0m "
+		return logging.Formatter.format(self, record)
+
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+formatter = MergerLogFormatter('%(module)s %(levelname) s%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
