8e3c592414690c138afe46637d9747fac7b92464
typoman <eslami.bahman@gmail.com>
2024-04-30 17:04:23 +0200

    [v 0.2.1.4] Add missing import for installing extension in RF

--- fontgadgets.roboFontExt/lib/installFontGadgets.py
+++ fontgadgets.roboFontExt/lib/installFontGadgets.py
@@ -6,3 +6,13 @@ This is the start up script for robofont.
 
 root = os.path.dirname(__file__)
 sys.path.append(root)
+
+import fontgadgets
+try:
+    # in py2
+    reload
+except NameError:
+    # in py3
+    from importlib import reload
+
+reload(fontgadgets)
