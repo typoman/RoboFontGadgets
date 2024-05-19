79817f4f46427e66cb70b7a2b6f4992ba6c185f6
typoman <eslami.bahman@gmail.com>
2024-05-19 22:12:38 +0200

    [glyph] method to copy glyph data separately.

--- Tests/test_glyph.py
+++ Tests/test_glyph.py
@@ -1,5 +1,6 @@
 from main import *
 import fontgadgets.extensions.glyph
+import operator
 
 def test_isComposite(defcon_ar_font_1):
     composite_names = "beh-ar.init khah-ar.init.calt hehgoalHamzaabove-ar.fina hah_alefMaksura-ar.fina.rlig gaf_yehHamzaabove-ar.rlig yehHamzaabove_meem-ar.init yehVabove_heh-ar.init".split()
@@ -67,3 +68,138 @@ def test_orderIndex(defcon_ar_font_1, glyphName, expectedOrder):
 def test_hasShape(defcon_ar_font_1, glyphName, expected):
     glyph = defcon_ar_font_1[glyphName]
     assert glyph.hasShape is expected
+
+@pytest.fixture(scope='function')
+def copy_source_glyph():
+    # sample is taken from defcon glyph test
+    source = defcon.Glyph()
+    source.name = "a"
+    source.width = 1
+    source.height = 2
+    source.unicodes = [3, 4]
+    source.note = "test image"
+    source.image = dict(fileName="test image", xScale=1, xyScale=1,
+                        yxScale=1, yScale=1, xOffset=0, yOffset=0,
+                        color=None)
+    source.anchors = [dict(x=100, y=200, name="test anchor")]
+    source.guidelines = [dict(x=10, y=20, name="test guideline")]
+    source.lib = {"foo": "bar"}
+    pen = source.getPointPen()
+    pen.beginPath()
+    pen.addPoint((100, 200), segmentType="line")
+    pen.addPoint((300, 400), segmentType="line")
+    pen.endPath()
+    component = defcon.Component()
+    component.base = "b"
+    source.appendComponent(component)
+    yield source
+
+
+@pytest.fixture(scope='function')
+def copy_target_glyph():
+    target = defcon.Glyph()
+    yield target
+
+def _contour_points_as_list(glyph):
+    result = []
+    for contour in glyph:
+        result.append([])
+        for point in contour:
+            result[-1].append((point.x, point.x,
+                point.segmentType, point.name))
+    return result
+
+def _get_operator(condition):
+    # Returns the equality operator (==) if condition is True, and the
+    # inequality operator (!=) if condition is False. This allows for dynamic
+    # switching between equality and inequality checks.
+    _operator = operator.eq if condition else operator.ne
+    return _operator
+
+def assert_changed_only(copy_target_glyph, copy_source_glyph, width=False,
+                     height=False, unicodes=False, note=False, image=False, contours=False,
+                     components=False, anchors=False, guidelines=False, lib=False):
+    # Assert that the attributes of copy_target_glyph are not changed
+    # from copy_source_glyph, except for the specified attributes.
+    assert _get_operator(width)(copy_target_glyph.width, copy_source_glyph.width)
+    assert _get_operator(height)(copy_target_glyph.height, copy_source_glyph.height)
+    assert _get_operator(unicodes)(copy_target_glyph.unicodes, copy_source_glyph.unicodes)
+    assert _get_operator(note)(copy_target_glyph.note, copy_source_glyph.note)
+    assert _get_operator(image)(copy_target_glyph.image, copy_source_glyph.image)
+    sourceContours = _contour_points_as_list(copy_source_glyph)
+    targetContours = _contour_points_as_list(copy_target_glyph)
+    assert _get_operator(contours)(sourceContours, targetContours)
+    assert _get_operator(components)(len(copy_target_glyph.components), len(copy_source_glyph.components))
+    assert _get_operator(anchors)([g.items() for g in copy_target_glyph.anchors], [g.items() for g in copy_source_glyph.anchors])
+    assert _get_operator(guidelines)([g.items() for g in copy_target_glyph.guidelines], [g.items() for g in copy_source_glyph.guidelines])
+    assert _get_operator(lib)(copy_target_glyph.lib, copy_source_glyph.lib)
+
+def test_copy_width(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=True, height=False, unicodes=False, note=False,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, width=True)
+
+def test_copy_height(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=True, unicodes=False, note=False,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, height=True)
+
+def test_copy_unicodes(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=True, note=False,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, unicodes=True)
+
+def test_copy_note(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=True,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, note=True)
+
+def test_copy_image(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=True, contours=False, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, image=True)
+
+def test_copy_contours(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=False, contours=True, components=False, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, contours=True)
+
+def test_copy_components(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=False, contours=False, components=True, anchors=False,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, components=True)
+
+def test_copy_anchors(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=False, contours=False, components=False, anchors=True,
+        guidelines=False, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, anchors=True)
+
+def test_copy_guidelines(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=True, lib=False)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, guidelines=True)
+
+def test_copy_lib(copy_target_glyph, copy_source_glyph):
+    copy_target_glyph.copyFromGlyph(
+        copy_source_glyph, width=False, height=False, unicodes=False, note=False,
+        image=False, contours=False, components=False, anchors=False,
+        guidelines=False, lib=True)
+    assert_changed_only(copy_target_glyph, copy_source_glyph, lib=True)
--- fontgadgets.roboFontExt/lib/fontgadgets/extensions/glyph/__init__.py
+++ fontgadgets.roboFontExt/lib/fontgadgets/extensions/glyph/__init__.py
@@ -1,6 +1,7 @@
 from fontgadgets.decorators import *
 import fontgadgets.extensions.component
 import fontgadgets.extensions.font
+from copy import deepcopy
 
 @font_property
 def isComposite(glyph):
@@ -38,3 +39,41 @@ def hasShape(glyph):
             if c.baseGlyph in font and font[c.baseGlyph].hasShape:
                 return True
     return False
+
+@font_method
+def copyFromGlyph(glyph, sourceGlyph, width=True, height=True, unicodes=True,
+             note=True, image=True, contours=True, components=True, anchors=True,
+             guidelines=True, lib=True):
+    """
+    Copy data from another **sourceGlyph**. This copies all the data by
+    defualt unless on of these argument is set to False:
+
+    width, height, unicodes, note, image, contours, components, anchors,
+    guidelines, lib
+    """
+    if width:
+        glyph.width = sourceGlyph.width
+    if height:
+        glyph.height = sourceGlyph.height
+    if unicodes:
+        glyph.unicodes = list(sourceGlyph.unicodes)
+    if note:
+        glyph.note = sourceGlyph.note
+    if image:
+        glyph.image = sourceGlyph.image
+    if contours:
+        for sourceContour in sourceGlyph:
+            c = glyph.instantiateContour()
+            c.setDataFromSerialization(sourceContour.getDataForSerialization())
+            glyph.appendContour(c)
+    if components:
+        for sourceComponent in sourceGlyph.components:
+            c = glyph.instantiateComponent()
+            c.setDataFromSerialization(sourceComponent.getDataForSerialization())
+            glyph.appendComponent(c)
+    if anchors:
+        glyph.anchors = [glyph.instantiateAnchor(a) for a in sourceGlyph.anchors]
+    if guidelines:
+        glyph.guidelines = [glyph.instantiateGuideline(g) for g in sourceGlyph.guidelines]
+    if lib:
+        glyph.lib = deepcopy(sourceGlyph.lib)
