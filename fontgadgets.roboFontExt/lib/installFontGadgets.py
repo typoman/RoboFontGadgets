d885f5afe694b5298fcd0936e97198995e74b496
Bahman Eslami <eslami.bahman@gmail.com>
2025-06-04 22:45:27 +0200

    [robofont.pip] add installer and use it for installing `bidi` and `git`

--- fontgadgets.roboFontExt/lib/installFontGadgets.py
+++ fontgadgets.roboFontExt/lib/installFontGadgets.py
@@ -1,5 +1,8 @@
 import sys
 import os
+import logging
+logger = logging.getLogger(__name__)
+
 """
 This is the start up script for robofont.
 """
@@ -17,6 +20,22 @@ except NameError:
     from importlib import reload
 
 reload(fontgadgets)
+
+pip_packages = {
+    "bidi": "python-bidi==0.4.2",
+    "git": "GitPython"
+}
+
+from fontgadgets.extensions.robofont.pip import installPIPPackage
+
+for import_name, package_spec in pip_packages.items():
+    try:
+        __import__(import_name)
+        logger.debug(f"Package '{package_spec}' (as '{import_name}') is already installed.")
+    except ImportError:
+        logger.warning(f"Package '{package_spec}' (as '{import_name}') not found. Attempting installation...")
+        if not installPIPPackage(package_spec):
+            logger.error(f"Installation of '{package_spec}' failed.")
 
 import fontgadgets.extensions.robofont.font
 import fontgadgets.extensions.robofont.tools
